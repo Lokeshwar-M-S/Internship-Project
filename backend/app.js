35a11f7d5a510ac4b6f7f014cef86a4c5f123f8b
Solitude-devil <lokeshwarloko@gmail.com>
2023-09-08 18:23:36 +0530

    modified

--- backend/app.js
+++ backend/app.js
@@ -4,54 +4,55 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-var cors = require('cors');
+var cors = require("cors");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error");
 
 //import routes
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const jobTypeRoute = require('./routes/jobTypeRoutes');
-const jobRoute = require('./routes/jobsRoutes');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const jobTypeRoute = require("./routes/jobTypeRoutes");
+const jobRoute = require("./routes/jobsRoutes");
 
 //Database
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-}).then(()=>console.log("Connected to DB"))
-    .catch((err) => console.log(err));
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 //Middleware
-app.use(morgan('dev'));
-app.use(bodyParser.json({limit: "5mb"}));
-app.use(bodyParser.urlencoded({
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
     limit: "5mb",
-    extended: true
-}));
+    extended: true,
+  })
+);
 app.use(cookieParser());
 app.use(cors());
-
 
 /*Routes
 app.get('/', (req, res) =>{
     res.send("Hello from Node js");
 })
 */
-app.use('/api',authRoutes);
-app.use('/api',userRoutes);
-app.use('/api', jobTypeRoute);
-app.use('/api', jobRoute);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", jobTypeRoute);
+app.use("/api", jobRoute);
 
 //error Middleware
-app.use(errorHandler)
-
-
+app.use(errorHandler);
 
 //port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, ()=>{
-    console.log(`Server running successfully on ${port}`);
+app.listen(port, () => {
+  console.log(`Server running successfully on ${port}`);
 });
