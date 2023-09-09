9d9e0b4dc129979a986a3217ef55555e5d50db08
Solitude-devil <lokeshwarloko@gmail.com>
2023-09-09 12:23:34 +0530

    modified and added some scripts

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
 
 const NotFound = () => {
   return (
     <>
+      <Navbar />
+      <Box
+        sx={{
+          height: "81vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <h1>Page not found!</h1>
+      </Box>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,15 +3,29 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { loadJobReducer } from "./reducers/jobReducer";
 import { loadJobTypeReducer } from "./reducers/jobTypeReducer";
+import {
+  userReducerLogout,
+  userReducerProfile,
+  userReducerSignIn,
+} from "./reducers/userReducer";
 
 //combine reducers
 const reducer = combineReducers({
   loadJobs: loadJobReducer,
   jobTypeAll: loadJobTypeReducer,
+  signIn: userReducerSignIn,
+  logOut: userReducerLogout,
+  userProfile: userReducerProfile,
 });
 
 //initial state
-let initialState = {};
+let initialState = {
+  signIn: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
+  },
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,
